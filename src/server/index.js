d66b224ecdbb1940a7288c511a9f2c8c347ca29d
Pham Thanh Tuan <phamthanhtuan100mc@gmail.com>
2023-02-05 16:15:40 +0900

    update server content

--- src/server/index.js
+++ src/server/index.js
@@ -1,25 +1,44 @@
-const io = require('socket.io')();
+const { createServer } = require('http');
+const fs = require('fs');
+const { Server } = require("socket.io");
 require('dotenv').config()
 
 const port = process.env.PORT || 3000;
 
-io.on('connection', (client) => {
-    console.log(client.rooms);
-    console.log('New client joined.');
-    console.log('Room count: ' + client.rooms.size + '\n');
-    io.emit('welcome');
-  
-    client.on("test", () => {
-      console.log("received test"); // not displayed
-      io.emit("ok");
-    })
-    
-    client.on("disconnect", () => {
-        console.log('Client left. Current total: ' + client.rooms.size);
+const httpServer = createServer((req, res) => {
+  fs.readFile('html/index.html', 'utf-8', (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.write('404: File Not Found');
+      return res.end();
+    }
 
-    });
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    return res.end();
   });
+
+});
+
+const io = new Server(httpServer);
+
+io.on('connection', (client) => {
+  console.log(client.rooms);
+  console.log('New client joined.');
+  console.log('Room count: ' + client.rooms.size + '\n');
+  io.emit('welcome');
+
+  client.on("test", () => {
+    console.log("received test"); // not displayed
+    io.emit("ok");
+  })
   
-  io.listen(port, () => {
-    console.log(`Server listening on port: ` + port);
+  client.on("disconnect", () => {
+      console.log('Client left. Current total: ' + client.rooms.size);
+
   });
+});
+
+httpServer.listen(port, () => {
+  console.log(`Server running at port: ${port}`);
+});
